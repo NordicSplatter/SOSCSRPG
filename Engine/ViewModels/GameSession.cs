3aef19b6f5c69d1ebe90e7e59169217c81e8a229
Cody Earhart <cearhart0205@gmail.com>
2020-01-03 10:09:25 -0600

    Data binding CurrentPlayer info from gamesession.cs to mainwindow.xaml

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -5,15 +5,19 @@ using Engine.Models;
 
 namespace Engine.ViewModels
 {
-    class GameSession
+    public class GameSession
     {
-        Player CurrentPlayer { get; set; }
+        public Player CurrentPlayer { get; set; }
 
         public GameSession()
         {
             CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Cody";
-            CurrentPlayer.Gold = 100000;
+            CurrentPlayer.Name = "Bob the Jittery";
+            CurrentPlayer.CharacterClass = "Berserker";
+            CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.HitPoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
